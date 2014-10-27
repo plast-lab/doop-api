9a20e9fb6f4caad30ddb8f4eb79739bd03a1af76
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-27 17:04:42 +0200

    Added fields to instruction beans

--- doop/EnterMonitorInstruction.java
+++ doop/EnterMonitorInstruction.java
@@ -1,5 +1,16 @@
 package doop;
 
 public class EnterMonitorInstruction extends MonitorInstruction {
+    private Var var = null;
 
+    public EnterMonitorInstruction(Var var) {
+        this.var = var;
+    }
+
+    public void setVar(Var var) {
+        this.var = var;
+    }
+    public Var getVar() {
+        return this.var;
+    }
 }
