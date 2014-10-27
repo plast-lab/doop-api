7c2b69997a1cf844de5c4e21cc758a889c00b2ad
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-27 16:05:44 +0200

    Added HeapAllocation bean

---
+++ doop/HeapAllocation.java
@@ -0,0 +1,26 @@
+package doop;
+
+/**
+ *
+ * @author
+ * anantoni
+ */
+public class HeapAllocation {
+    Type type = null;
+
+    public HeapAllocation(Type type) {
+        this.type = type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        retun type;
+    }
+    //TODO: I don't think this is the correct return type
+    public HeapAllocation getNullObject {
+        return null;
+    }
+}
