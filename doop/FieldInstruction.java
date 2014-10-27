cecacd405e2b616b5744819df0d110d4dc5d6e27
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-27 17:23:00 +0200

    fixes in LoadInstanceFieldInstruction bean

--- doop/FieldInstruction.java
+++ doop/FieldInstruction.java
@@ -1,6 +1,21 @@
 package doop;
 
 public class FieldInstruction extends Instruction {
-    
+    private FieldSignature signature;
+    private Var base;
+    private Var to;
 
+    public FieldInstruction(FieldSignature signature, Var base, Var to) {
+        this.signature = signature;
+        this.base = base;
+        this.to = to;
+    }
+
+    public void setSignature(FieldSignature signature) {
+        this.signature = signature;
+    }
+
+    public FieldSignature getSignature() {
+        return this.signature;
+    }
 }
