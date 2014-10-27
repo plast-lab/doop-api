f6bb4f012e1244f9fe7d7403ac5a14cfb874f4cd
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-27 23:45:02 +0200

    Adding ReferenceType class.

---
+++ doop/ReferenceType.java
@@ -0,0 +1,25 @@
+package doop;
+
+/**
+ *
+ * @author anantoni
+ */
+
+public class ReferenceType extends Type {
+
+    /** Constructs a reference type. */
+    public ReferenceType() {
+        super();
+        this();
+    }
+
+
+    /** Constructs a reference type with a fully qualified name.
+     *
+     * @param fullyQualifiedName  the fully qualified name of the reference type
+     */
+    public ReferenceType(String fullyQualifiedName) {
+        super(fullyQualifiedName);
+        this();
+    }
+}
