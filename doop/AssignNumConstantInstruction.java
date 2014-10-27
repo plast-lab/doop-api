605c85b992667139b90efc028a8d0de123cd3622
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-27 18:31:24 +0200

    Changes in AssignNumConstantInstruction bean

--- doop/AssignNumConstantInstruction.java
+++ doop/AssignNumConstantInstruction.java
@@ -1,5 +1,17 @@
 package doop;
 
 public class AssignNumConstantInstruction extends AssignInstruction {
+    private NumConstant value = null;
 
+    public class AssignNumConstantInstruction(NumConstant value) {
+        this.value = value;
+    }
+
+    public void setValue(NumConstant value) {
+        this.value = value;
+    }
+
+    public NumConstant getValue() {
+        return this.value;
+    }
 }
