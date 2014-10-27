6be9af9cb6dff8fd6868bed2f471c8919f80af86
George Balatsouras <gbalats@gmail.com>
2014-10-27 20:01:32 +0200

    Adding some javadoc on the Instruction class.

--- doop/Instruction.java
+++ doop/Instruction.java
@@ -3,40 +3,122 @@ package doop;
 /**
  *
  * @author anantoni
+ * @author gbalats
  */
-public class Instruction {
-    private String value = null;
-    private int index = -1;     //TODO: is this ok?
-    private MethodSignature inmethod = null;
+public class Instruction
+{
+    private String refmode;
+    private int index = -1;
+    private MethodSignature enclosingMethod;
 
-    public Instruction(String value, int index, MethodSignature inmethod) {
-        this.value = value;
+
+    /** Constructs an instruction. */
+    public Instruction()
+    {
+        this();
+    }
+
+
+    /**
+     * Constructs an instruction with a refmode.
+     *
+     * @param refmode  the refmode of this instruction
+     */
+    public Instruction(String refmode)
+    {
+        this(refmode,  null);
+    }
+
+
+    /**
+     * Constructs an instruction with a refmode and an index.
+     *
+     * @param refmode  the refmode of this instruction
+     * @param index  the index of this instruction
+     */
+    public Instruction(String refmode, int index)
+    {
+        this(refmode, index, null);
+    }
+
+
+    /**
+     * Constructs an instruction with a refmode, an index, and an
+     * enclosing method.
+     *
+     * @param refmode          the refmode of this instruction
+     * @param index            the index of this instruction
+     * @param enclosingMethod  the signature of this instruction's enclosing method
+     */
+    public Instruction(String refmode, int index, MethodSignature enclosingMethod)
+    {
+        this.refmode = refmode;
         this.index = index;
-        this.inmethod = inmethod;
+        this.enclosingMethod = enclosingMethod;
     }
 
-    public void setValue(String value) {
-        this.value = value;
+
+    /**
+     * Sets the refmode of this instruction.
+     *
+     * @param refmode  the refmode to set
+     */
+    public void setRefmode(String refmode) {
+        this.refmode = refmode;
     }
 
-    public String getValue() {
-        return this.value;
+
+    /**
+     * Returns the <i>refmode</i> of this instruction.
+     *
+     * @return  the <i>refmode</i> of this instruction
+     */
+    public String getRefmode() {
+        return this.refmode;
     }
 
+
+    /**
+     * Sets the unique index of this instruction in its enclosing
+     * method.
+     *
+     * @param index  the unique index to set
+     */
     public void setIndex(int index) {
         this.index = index;
     }
 
+
+    /**
+     * Returns the unique index of this instruction in its enclosing
+     * method.
+     *
+     * @return  the unique index of this instruction
+     */
     public int getIndex() {
         return this.index;
     }
 
-    public void setInMethod(MethodSignature inmethod) {
-        this.inmethod = inmethod;
+
+    /**
+     * Sets the enclosing method of this instruction.
+     *
+     * @param enclosingMethod the signature of the enclosing method to
+     * set
+     */
+    public void setEnclosingMethod(MethodSignature enclosingMethod) {
+        this.enclosingMethod = enclosingMethod;
     }
 
-    public MethodSignature getInMethod() {
-        return inmethod;
 
+    /**
+     * Returns the signature of the enclosing method of this
+     * instruction.
+     *
+     * @return the signature of the enclosing method of this
+     * instruction
+     */
+    public MethodSignature getEnclosingMethod() {
+        return enclosingMethod;
     }
 }
