2ba73a7ddf09a9f6a76c10b3153fc43e7cf92bf1
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-30 01:23:59 +0200

    Refactoring subclasses of the Instruction class.

--- doop/ArrayInstruction.java
+++ doop/ArrayInstruction.java
@@ -1,5 +1,46 @@
 package doop;
 
 public class ArrayInstruction extends Instruction {
-    
+    /** Constructs an array instruction. */
+    public ArrayInstruction()
+    {
+        super();
+    }
+
+
+    /**
+     * Constructs an array instruction with a refmode.
+     *
+     * @param refmode  the refmode of this array instruction
+     */
+    public ArrayInstruction(String refmode)
+    {
+        super(refmode,  null);
+    }
+
+
+    /**
+     * Constructs an array instruction with a refmode and an index.
+     *
+     * @param refmode  the refmode of this array instruction
+     * @param index  the index of this array instruction
+     */
+    public ArrayInstruction(String refmode, int index)
+    {
+        super(refmode, index, null);
+    }
+
+
+    /**
+     * Constructs an array instruction with a refmode, an index, and an
+     * enclosing method.
+     *
+     * @param refmode          the refmode of this array instruction
+     * @param index            the index of this array instruction
+     * @param enclosingMethod  the signature of this array instruction's enclosing method
+     */
+    public ArrayInstruction(String refmode, int index, MethodSignature enclosingMethod)
+    {
+        super(refmode, index, enclosingMethod);
+    }
 }
--- doop/ThrowInstruction.java
+++ doop/ThrowInstruction.java
@@ -2,4 +2,48 @@ package doop;
 
 public class ThrowInstruction extends Instruction {
 
+
+    /** Constructs a throw instruction. */
+    public ThrowInstruction()
+    {
+        super();
+    }
+
+
+    /**
+     * Constructs a throw instruction with a refmode.
+     *
+     * @param refmode  the refmode of this throw instruction
+     */
+    public ThrowInstruction(String refmode)
+    {
+        super(refmode,  null);
+    }
+
+
+    /**
+     * Constructs a throw instruction with a refmode and an index.
+     *
+     * @param refmode  the refmode of this throw instruction
+     * @param index  the index of this throw instruction
+     */
+    public ThrowInstruction(String refmode, int index)
+    {
+        super(refmode, index, null);
+    }
+
+
+    /**
+     * Constructs a throw instruction with a refmode, an index, and an
+     * enclosing method.
+     *
+     * @param refmode          the refmode of this throw instruction
+     * @param index            the index of this throw instruction
+     * @param enclosingMethod  the signature of this throw instruction's enclosing method
+     */
+    public ThrowInstruction(String refmode, int index, MethodSignature enclosingMethod)
+    {
+        super(refmode, index, enclosingMethod);
+    }
+
 }
